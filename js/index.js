59e854ff659af0176476327d329f00aa3f3d28f7
Kaneki3332 <gabi.mihnea46@gmail.com>
2026-02-19 19:22:51 +0200

    passenger counter app

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
+saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-console.log(countEl);
+console.log(saveEl);
 let count = 0;
 function increment() {
   count++;
-  countEl.innerText = count;
+  countEl.textContent = count;
+}
+function save() {
+  let prev = count + " - ";
+  saveEl.textContent += prev;
+  countEl.textContent = 0;
+  count = 0;
 }
